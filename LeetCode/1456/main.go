4d624fe809404445bcc5596c27bbeab8a2550830
Sandro Müller Scheuermann <sandroryze@gmail.com>
2023-09-06 23:33:30 -0300

    sliding window dos guris

--- LeetCode/1456/main.go
+++ LeetCode/1456/main.go
@@ -1,8 +1,55 @@
-package leetcode
+package main
 
-import ()
+import (
+	"fmt"
+)
 
-func main(){
-
-
+func main() {
+	fmt.Print(maxVowels("aeiou", 2))
 }
+
+var vogais = map[rune]bool{
+
+    'a': true,
+    'e': true,
+    'i': true,
+    'o': true,
+    'u': true,
+}
+
+func ehVogal(letra rune) bool{
+    return vogais[letra]
+}
+
+func maxVowels(s string, k int) int {
+
+    contadorMax, contadorJanela := 0, 0
+
+    stringInicial := s[:k] 
+
+    for _, letra := range stringInicial{
+        if ehVogal(letra){
+            contadorJanela++
+        }
+    }
+
+    contadorMax = contadorJanela
+
+    for i := k; i < len(s) ; i++{
+
+        if ehVogal(rune(s[i-k])){
+            contadorJanela--
+        }
+
+        if ehVogal(rune(s[i])){
+            contadorJanela++ 
+        }
+
+        if(contadorJanela > contadorMax){
+            contadorMax = contadorJanela
+        }
+    }
+
+	return contadorMax 
+}
+
